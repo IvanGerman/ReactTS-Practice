df2cb182d2e99f85d322284a6bb7318a34394f4c
IvanGerman <ivgvi@mail.ru>
2024-03-13 19:35:12 +0700

    work: adding simple state file with items data and ts-types file

---
+++ src/state.ts
@@ -0,0 +1,12 @@
+export const state = {
+  ugolki: [
+    { name: 'Уголок усилённый 50*50*35', linkToImage : '12345.png', linkToVideo : '12345.mp4', amount : 200, price : 9 },
+    { name: 'Уголок  60*60*50', linkToImage : '99345.png', linkToVideo : '99345.mp4', amount : 100, price : 13 },
+  ],
+  samorezi: [
+    { name: 'Саморез чёрный 3,5*41', linkToImage : '77345.png', linkToVideo : '77345.mp4', amount : 1200, price : 0.5 },
+  ],
+  others: [
+    { name: 'Опора для бруса 140*50*76', linkToImage : '33345.png', linkToVideo : '33345.mp4', amount : 20, price : 70 },
+  ]
+}
