0355e17bb379bb97681672fcbfed2f85d82ef7b6
IvanGerman <ivgvi@mail.ru>
2024-03-28 22:22:00 +0700

    feat: add last data for samorezi component

--- src/state.ts
+++ src/state.ts
@@ -14,12 +14,12 @@ export const state: StateType = {
   samorezi: [
     { name: 'Саморез чёрный 3,5*36 мм', linkToImage : Image1, linkToVideo : 'https://www.youtube.com/embed/-hZ6cJWZu4Y', amount : 1785, amountKG : 3, price : 50, priceKG : 270, numberInKG : 595 },
     { name: 'Саморез чёрный 3,5*41 мм', linkToImage : Image2, linkToVideo : 'https://www.youtube.com/embed/H-_RIa4RzLA', amount : 1575, amountKG: 3.5, price : 60, priceKG : 270, numberInKG : 450 },
-    { name: 'Саморез чёрный 4,2*64 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/Y91EVb6C4lg', amount : 776, amountKG: 2, price : 70, priceKG : 270, numberInKG : 388 },
-    { name: 'Саморез чёрный 4,2*70 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/w6wwp1DvTT0', amount : 400, amountKG: 2, price : 150, priceKG : 300, numberInKG : 200 },
-    { name: 'Саморез оцинкованный 5*50 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/9Rlzjtb6hj4', amount : 800, amountKG: 3, price : 0.8, priceKG : 270, numberInKG : 595 },
-    { name: 'Саморез оцинкованный 4*70 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/KYx0XOTjmJM', amount : 800, amountKG: 3, price : 0.8, priceKG : 270, numberInKG : 595 },
-    { name: 'Саморез оцинкованный 4,2*76 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/hwiUdGWaI3M', amount : 800, amountKG: 3, price : 0.8, priceKG : 270, numberInKG : 595 },
-    { name: 'Саморез оцинкованный 6*160 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/evfHIqGOkUw', amount : 800, amountKG: 3, price : 0.8, priceKG : 270, numberInKG : 595 },
+    { name: 'Саморез чёрный 4*64 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/Y91EVb6C4lg', amount : 776, amountKG: 2, price : 70, priceKG : 270, numberInKG : 388 },
+    { name: 'Саморез чёрный 4,5*70 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/w6wwp1DvTT0', amount : 400, amountKG: 2, price : 150, priceKG : 300, numberInKG : 200 },
+    { name: 'Саморез оцинкованный 5*50 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/9Rlzjtb6hj4', amount : 735, amountKG: 2.5, price : 120, priceKG : 350, numberInKG : 295 },
+    { name: 'Саморез оцинкованный 4*70 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/KYx0XOTjmJM', amount : 340, amountKG: 1, price : 100, priceKG : 350, numberInKG : 345 },
+    { name: 'Саморез оцинкованный 4,2*76 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/hwiUdGWaI3M', amount : 260, amountKG: 1.4, price : 190, priceKG : 350, numberInKG : 185 },
+    { name: 'Саморез оцинкованный 6*160 мм', linkToImage : Image3, linkToVideo : 'https://www.youtube.com/embed/evfHIqGOkUw', amount : 40, amountKG: 0.63, price : 555, priceKG : 350, numberInKG : 63 },
     
   ],
   others: [
